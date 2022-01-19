ac954210fef07fd9cfad2e0509aa5fe2d6099797
karenbarseghyan90@gmail.com <karenbarseghyan90@gmail.com>
2022-01-19 17:24:19 +0400

    InputHandler for Class-Components

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -3,14 +3,38 @@ import React, { Component } from 'react'
 export class PostForm extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            title: ""
+        }
     }
     
+    submitHandler = (ev) => {
+        ev.preventDefault();
+        console.log(this.state.title)
+    }
+
+    changeInputHandler = (ev) => {
+        this.setState(prev => ({...prev,...{
+            [ev.target.name]:ev.target.value
+        }}))
+    }
+
     render() {
         return (
-            <div>
-                <h1>Post-Form</h1>
-            </div>
+            <form onSubmit={this.submitHandler}>
+                <div className="form-group">
+                    <label htmlFor="title">Post Title </label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="title" 
+                        value={this.state.title}
+                        name="title"
+                        onChange={this.changeInputHandler}
+                    />
+                </div>
+                <button className="btn btn-success" type="submit">Create</button>
+            </form>
         )
     }
 }
